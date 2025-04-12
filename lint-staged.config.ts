b7612d35ff199e1eb8a81bc6afaeb80adaea7a11
SpanishPear <shrey.somaiya@gmail.com>
2025-04-12 23:40:37 +1000

    fix: litn-taged syntax

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -2,7 +2,7 @@ import type { Configuration } from 'lint-staged'
 
 const config: Configuration = {
 	'*.{js,jsx,ts,tsx}': (stagedFiles) => `oxlint --fix ${stagedFiles.join(' ')}`,
-	'*.{js|ts|tsx|yml|yaml|md|json}': (stagedFiles) => `prettier --write ${stagedFiles.join(' ')}`,
+	'*.{js,ts,tsx,yml,yaml,md,json}': (stagedFiles) => `prettier --write ${stagedFiles.join(' ')}`,
 	'*.toml': (stagedFiles) => `taplo format ${stagedFiles.join(' ')}`,
 	'*.rs': () => ['cargo fmt --all', 'cargo clippy --all-targets --all-features -- -D warnings'],
 }
