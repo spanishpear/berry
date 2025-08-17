27c292db065373ba2271f48725386a59ba2e21c1
SpanishPear <shrey.somaiya@gmail.com>
2025-08-18 00:15:34 +1000

    fix test and build - i hope

--- crates/node-bindings/__test__/index.spec.ts
+++ crates/node-bindings/__test__/index.spec.ts
@@ -5,7 +5,8 @@ import { parse } from '../index'
 
 test('can parse a file', (t) => {
 	// cwd is actually not the test dir, but the root of the project
-	const fileContents = fs.readFileSync('../../fixtures/berry.lock', 'utf-8')
+	// needs us to be buffered
+	const fileContents = fs.readFileSync('../../fixtures/berry.lock')
 	const parsed = parse(fileContents)
 	t.log(`result is ${parsed.length} bytes long`)
 	t.true(true)
